6bd26c117ea73be5e1d863bc7203d5bb1ce88f5e
raxonpc <tomeczekorlowski1@gmail.com>
2022-07-26 14:25:06 +0200

    Basic ls implementation (only for directories)

--- rx_ls.c
+++ rx_ls.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <dirent.h>
+
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    const char* dir_name = 
+        (argc > 1) ? argv[1] : ".";
+
+    DIR *dir = opendir(dir_name);
+    if(dir == NULL)
+    {
+        fprintf(stderr,
+                "cannot access '%s': No such directory\n", dir_name);
+        return 1;
+    }
+
+    struct dirent *directory;
+    while((directory = readdir(dir)) != NULL)
+    {
+        printf("%s ", directory->d_name);
+    }
+    putchar('\n');
+    int temp_close = closedir(dir);
+    if(temp_close == -1)
+    {
+        fprintf(stderr,
+        "Error closing directory %s\n", dir_name);
+        return 1;
+    }
+
     return 0;
 }
 
